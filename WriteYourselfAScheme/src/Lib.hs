b876fe2e546780f29268a60d1f4363c37f789ad1
mt-yohei-koyama <ykoyama@moneytree.jp>
2021-06-25 12:33:32 +0900

    chore: add some fix

--- WriteYourselfAScheme/src/Lib.hs
+++ WriteYourselfAScheme/src/Lib.hs
@@ -1,5 +1,6 @@
-module Lib ( someFunc, readExpr) where
+module Lib (readExpr) where
 
+import Control.Monad
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
@@ -14,6 +15,29 @@ readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
